89ecfb1357f321bb6a432aeca1f182d2bfbda801
samtmorgan <sam.morgan@beacon-tech.co.uk>
2023-10-16 18:16:33 +0100

    perf: improve tests

--- src/constants/copy.ts
+++ src/constants/copy.ts
@@ -1,27 +1,2 @@
-import { WeatherStatus } from '@/types/types';
-import { DENIED, ERROR, LOADING, NOT_INITIALIZED, SUCCESS } from './statuses';
-
-export const WELCOME_MESSAGE = 'Welcome to Super Potato!';
-// export const LOGIN_MESSAGE = 'Please log in to continue.';
-// export const SIGNUP_MESSAGE = "Don't have an account? Sign up now!";
-// export const USERNAME_LABEL = 'Username';
-// export const PASSWORD_LABEL = 'Password';
-// export const LOGIN_BUTTON_TEXT = 'Log In';
-// export const SIGNUP_BUTTON_TEXT = 'Sign Up';
-// export const FORGOT_PASSWORD_TEXT = 'Forgot Password?';
-export const ERROR_MESSAGE = 'Oops! Something went wrong. Please try again.';
-
-export const CurrentWeatherMessages = {
-  [NOT_INITIALIZED]: 'Press use location to get the weather.',
-  [LOADING]: 'Loading...',
-  [SUCCESS]: 'The weather is currently',
-  [ERROR]: 'Oops, there was a problem getting the weather, try again.',
-};
-
-export function getCurrentWeatherMessage(status: WeatherStatus) {
-  return CurrentWeatherMessages[status];
-}
-
-export const locationStatusMessages = {
-  [DENIED]: `We don't have access to your location data, check your browser or device settings`,
-};
+export const pageContentError = 'Something went wrong getting the weather, please try again';
+export const pageContentNavDenied = 'Please grant permission for us to use your location in browser or device settings';
