27ddc6f34ae021bde4b8af51782fce2ea1f0c571
samtmorgan <sam.morgan@beacon-tech.co.uk>
2023-11-02 09:04:34 +0000

    feat: improve search results UI

--- src/components/input/LocationInput.tsx
+++ src/components/input/LocationInput.tsx
@@ -5,7 +5,8 @@ import { Tooltip } from 'react-tooltip';
 import { LiaLocationArrowSolid } from 'react-icons/lia';
 import { AiOutlineClose } from 'react-icons/ai';
 import { v4 as uuidv4 } from 'uuid';
-import { Suggestion } from '@/types/types';
+import { SearchResult, Suggestion } from '@/types/types';
+import { getWeatherForSearchResult } from '@/api/api';
 
 const sessionId = uuidv4();
 
@@ -21,14 +22,42 @@ function buildUrl(query: string): string {
 }
 
 export function LocationInput() {
-  const { searchValue, setSearchValue, setCoords, locationStatus, setLocationStatus, setSearchResults } =
-    useAppContext();
+  const {
+    searchValue,
+    setSearchValue,
+    setCoords,
+    locationStatus,
+    setLocationStatus,
+    setSearchResults,
+    searchResults,
+    setAddress,
+    setWeatherAssets,
+    setWeatherStatus,
+  } = useAppContext();
+
+  const handleClickSearchResult = useCallback(
+    (searchResult: SearchResult) => {
+      setSearchValue('');
+      const searchStr = searchResult.text;
+      getWeatherForSearchResult(
+        searchStr,
+        searchResult.type,
+        setAddress,
+        setWeatherAssets,
+        setWeatherStatus,
+        setLocationStatus,
+      );
+      setSearchResults(null);
+    },
+    [setSearchValue, setAddress, setLocationStatus, setSearchResults, setWeatherAssets, setWeatherStatus],
+  );
 
   const getLocation = useCallback(() => {
     setLocationStatus(LOADING);
     try {
       navigator.geolocation.getCurrentPosition(
         position => {
+          setSearchResults(null);
           setLocationStatus(SUCCESS);
           setCoords({ lat: position.coords.latitude, lng: position.coords.longitude });
         },
@@ -39,7 +68,7 @@ export function LocationInput() {
     } catch (error) {
       setLocationStatus(ERROR);
     }
-  }, [setCoords, setLocationStatus]);
+  }, [setCoords, setLocationStatus, setSearchResults]);
 
   useEffect(() => {
     if (searchValue) {
@@ -64,92 +93,125 @@ export function LocationInput() {
   return (
     <form>
       <div
-        className={`
-        w-full
-        h-10 
-        flex 
-        gap-2 
-        items-center 
-        justify-between  
-        border-slate-800
-        border
-        p-px
-        focus:outline-0
-        active:outline-0
-        disabled:hover:border-1
-        disabled:hover:border-slate-400
-        disabled:hover:border
-        hover:border-2
-        rounded-full
-        hover:disabled:bg-transparent
-        hover:p-0
-        mb-5
-        `}
+        className="
+                flex 
+                flex-col                
+                w-full
+                flex 
+                gap-2 
+                items-center 
+                justify-between  
+                pt-1
+                pb-1
+                p-1
+                focus:outline-0
+                active:outline-0
+                disabled:hover:outline-1
+                disabled:hover:outline-slate-400
+                disabled:hover:outline
+                rounded-lg
+                hover:disabled:bg-transparent
+                mb-5
+                outline
+                outline-slate-800
+                outline-1
+                hover:outline-2  
+                "
       >
-        <input
-          className={`
+        <div className="flex">
+          <input
+            className={`
             bg-transparent
             focus:outline-0
             active:outline-0
             ml-5
             w-full
             text-base
-        `}
-          aria-label="Search"
-          //   id="location"
-          name="Search"
-          type="text"
-          placeholder="Search"
-          onChange={e => {
-            e.preventDefault();
-            setSearchValue(e.target.value);
-          }}
-          value={searchValue}
-        />
 
-        <button
-          aria-label="clear search"
-          data-tooltip-id="clear-search-tooltip"
-          data-tooltip-content="Clear search"
-          type="button"
-          disabled={searchValue === ''}
-          className={`
-        flex justify-center items-center
-        h-8
-        w-8
-        hover:bg-sky-400/30 
-        active:bg-sky-600/30
-        disabled:text-slate-400
-        disabled:bg-transparent
-        rounded-full
-        p-2 
         `}
-          onClick={() => setSearchValue('')}
-        >
-          <AiOutlineClose />
-        </button>
-        <button
-          aria-label="get location"
-          data-tooltip-id="locate-tooltip"
-          data-tooltip-content="Current location"
-          type="button"
-          disabled={locationStatus === LOADING || locationStatus === DENIED}
-          className={`
-        flex justify-center items-center
-        h-8
-        w-8
-        hover:bg-sky-400/30 
-        active:bg-sky-600/30
-        disabled:text-slate-400
-        disabled:bg-transparent
-        rounded-full
-        p-2 
-        mr-0.5
-        `}
-          onClick={getLocation}
-        >
-          <LiaLocationArrowSolid />
-        </button>
+            aria-label="Search"
+            //   id="location"
+            name="Search"
+            type="text"
+            placeholder="Search"
+            onChange={e => {
+              e.preventDefault();
+              setSearchValue(e.target.value);
+            }}
+            value={searchValue}
+          />
+          <button
+            aria-label="clear search"
+            data-tooltip-id="clear-search-tooltip"
+            data-tooltip-content="Clear search"
+            type="button"
+            disabled={searchValue === ''}
+            className={`
+                flex justify-center items-center
+                h-8
+                w-8
+                hover:bg-sky-400/30 
+                active:bg-sky-600/30
+                disabled:text-slate-400
+                disabled:bg-transparent
+                rounded-full
+                p-2 
+                `}
+            onClick={() => setSearchValue('')}
+          >
+            <AiOutlineClose />
+          </button>
+          <button
+            aria-label="get location"
+            data-tooltip-id="locate-tooltip"
+            data-tooltip-content="Current location"
+            type="button"
+            disabled={locationStatus === LOADING || locationStatus === DENIED}
+            className={`
+                flex justify-center items-center
+                h-8
+                w-8
+                hover:bg-sky-400/30 
+                active:bg-sky-600/30
+                disabled:text-slate-400
+                disabled:bg-transparent
+                rounded-full
+                p-2 
+                mr-0.5
+                `}
+            onClick={getLocation}
+          >
+            <LiaLocationArrowSolid />
+          </button>
+        </div>
+        {searchResults && (
+          <menu
+            className="
+              border-slate-400
+              border-t
+            divide-y
+            divide-slate-400
+            w-full
+            "
+          >
+            {searchResults.map(searchResult => (
+              <li
+                className="
+                flex
+                items-center
+                justify-center
+                h-12
+                hover:bg-sky-300/30
+            "
+                key={searchResult.text}
+              >
+                <button onClick={() => handleClickSearchResult(searchResult)} className="w-full h-full" type="button">
+                  {searchResult.text}
+                </button>
+              </li>
+            ))}
+          </menu>
+        )}
       </div>
       <Tooltip id="locate-tooltip" />
     </form>
